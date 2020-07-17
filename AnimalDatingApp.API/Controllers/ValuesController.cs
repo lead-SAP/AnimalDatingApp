92d21e015d7a299b392322039d5afbf63f4c19c1
عمرو عبدالله <captainamrabdullah@gmail.com>
2020-07-17 15:14:11 +0300

    EOF 4

--- AnimalDatingApp.API/Controllers/ValuesController.cs
+++ AnimalDatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace AnimalDatingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
