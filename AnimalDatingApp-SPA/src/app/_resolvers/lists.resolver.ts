4743a6ea7bc4968ab3f3688417a7c6ff99008676
عمرو عبدالله <captainamrabdullah@gmail.com>
2020-09-20 03:04:47 +0300

    Pre-Publishing

--- AnimalDatingApp-SPA/src/app/_resolvers/lists.resolver.ts
+++ AnimalDatingApp-SPA/src/app/_resolvers/lists.resolver.ts
@@ -15,7 +15,6 @@ export class ListsResolver implements Resolve<User[]>{
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService){}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]>{
-        // tslint:disable-next-line: no-string-literal
         return this.userService.getUsers(this.pageNumber, this.pageSize, null, this.likesParam).pipe(
             catchError(error => {
                 this.alertify.error('Problem retrieving data');
